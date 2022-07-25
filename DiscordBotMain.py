a9bd1ad8c87a5fe6d50671278578289653551362
JXLK-dev <minemaster151@gmail.com>
2022-07-25 22:41:54 +0700

    change

--- DiscordBotMain.py
+++ DiscordBotMain.py
@@ -2,4 +2,21 @@ import discord
 from discord.ext import commands
 import os
 
-bot = commands.Bot(command_prefix='!', case_insensitive=True)
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    # db.connectDatabase()
+    print("I'm online")
+
+
+# @bot.command()
+# async def isGay(ctx, *, funny):
+#     if(funny == "Soham"):
+#         await ctx.send("%s is gay" % funny)
+#     else:
+#         await ctx.send("%s is straight" % funny)
+
+
+bot.run(os.getenv('TOKEN'))
