abe5ddd03b63fa7c6b97c04cc34606d1d487f32b
JXLK-dev <minemaster151@gmail.com>
2022-07-28 00:07:52 +0700

    refactoring things

--- DiscordBotMain.py
+++ DiscordBotMain.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 import os
 import lyricsgenius
 from LoopSystem import Loop_System
+from QueueSystem.Queue_System import QueueSystem as qs
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 geniusKey = "URYL2IOIx69b2C43KbAKNCGLhAa9yY-bidzUdn3TILjI1MNFu2GlLUFh-0xmWDZk"
@@ -16,35 +17,5 @@ genius = lyricsgenius.Genius(geniusKey)
 async def on_ready():
     print("I'm online")
 
-
-@bot.command()
-async def isGay(ctx, *, funny):
-    if(funny == "Soham"):
-        await ctx.send("%s is gay" % funny)
-    else:
-        await ctx.send("%s is straight" % funny)
-
-
-@bot.command()
-async def join(ctx):
-    channel = ctx.author.voice.channel
-    await channel.connect()
-
-
-@bot.command()
-async def leave(ctx):
-    await ctx.voice_client.disconnect()
-
-
-@bot.command()
-async def lyricfind(ctx, *, query):
-    details = query.split("#")
-    artist = genius.search_artist(details[0], max_songs=0)
-    song = artist.song(details[1])
-    if(song.lyrics == NULL):
-        await ctx.send("Lyric failed to load")
-    else:
-        await ctx.channel.send(song.lyrics)
-    return
 
 bot.run('MTAwMDc3NzAxMDg2NTUwNDI1Ng.GN2UCH.AoaqRqwP5n4_CozqdbwbwZcTK_qjF3aqrKKsiA')
--- QueueSystem/Queue_System.py
+++ QueueSystem/Queue_System.py
@@ -2,5 +2,9 @@ class QueueSystem:
     def __init__(self) -> None:
         pass
 
-    def add(song):
+    def add(ctx, funny):
+        if(funny == "Soham"):
+            ctx.send("%s is gay" % funny)
+        else:
+            ctx.send("%s is straight" % funny)
         return
